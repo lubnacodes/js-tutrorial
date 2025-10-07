0eec32a556c2aa92d8df807717929b78eb4ffb6c
Lubnacoder <khanlubna34069@gmail.com>
2025-10-07 21:03:01 +0000

    basics/02_datatypes.js

---
+++ basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version when we write this 
+//alert(3 + 3) // we can use alert with different syntax when we are using nodejs not browser
+
+console.log(3+3); 
+
+console.log("lubna")
+
+let name = "lubna" //string
+let age = 25 // number 
+let islogedIn = false //boolean
+
+//number - range - 2 to power 53
+//bigint 
+//string  = ""
+//boolean = true/false
+//null = standlone value  it like empty nut not 0
+// undefined  when we declare a variable but not define its value 
+//symbol use for find uniqeness 
+
+//object 
+console.log(typeof undefined)
+
+
