dd9361471b71bf643d9fcbf9e3bd91ba1313fda6
Lubnacoder <khanlubna34069@gmail.com>
2025-10-09 20:53:33 +0000

    basics/05_datatypessummary

---
+++ basics/05_datatypessummary.js
@@ -0,0 +1,56 @@
+// primitive not premitive 
+// in js datypes categories in two way in base  of how data store in memory
+ 
+
+// primitive  (call by value )  these are 7 types
+// String, Number, Boolean, null, undefined, Symbol, BigiInt
+const score  = 100
+const scorevalue = 100.5
+
+const isloggedIn = false 
+const outsidetemp = null
+let userEmail ; // undefined 
+
+const id = Symbol('123')
+const anotherID = Symbol('123');
+console.log(anotherID);
+
+//const bigNumber = 56565656564896565n;
+const bigNumber = BigInt(56565656564896565);
+console.log(typeof(bigNumber));
+
+
+// Reference type or Non premitive 
+// Array,  Objects, Funcitons
+
+//1 array
+const cars = ["toyota fortuner", "maruti", "honda city", "bugati"];
+console.log(cars);
+
+// 2 obeject
+let myobj = {
+    name : "lubna",
+    age : 22,
+    phone : '564565663',
+    email : "l@gmail.com"
+}
+console.log(myobj);
+
+//3 function 
+
+function myfunc()
+{
+ console.log("hello lubna");
+}
+myfunc();
+
+const myfunc = function ()
+{
+    console.log("hello lubna");
+}
+
+
+
+
+
+
