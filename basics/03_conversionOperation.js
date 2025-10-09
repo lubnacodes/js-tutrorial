acec72b24b42c2df7fe0ca91c32b8442888fca04
Lubnacoder <khanlubna34069@gmail.com>
2025-10-09 03:43:37 +0000

    basics/03_conversionOperation.js

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33abc" // here let is a variable with a value 
+
+// but when we work with backend and get value by user (form) so value can be ohter formate 
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);
+
+// "33" easily converted into 33 but ,
+// 33abc  will be NaN 
+// true = 1;  false = 0 
+
+let isloggedIn = "lubna"
+
+let booleanIsLoggendIn = Boolean(isloggedIn)
+console.log(booleanIsLoggendIn);
+
+// when write one(1) it convert into true and 0 or empty is false
+
+let someNumbber = 33
+let StringNumber = String(someNumbber)
+console.log(typeof(StringNumber))
