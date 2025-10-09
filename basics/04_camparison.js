dad196ebeee860672d55a06fba48a2c77189b3e7
Lubnacoder <khanlubna34069@gmail.com>
2025-10-09 20:20:40 +0000

    basics/04_camparison.js

---
+++ basics/04_camparison.js
@@ -0,0 +1,21 @@
+//console.log(2 < 1)//less then
+//console.log(2 > 1)//greater then
+//console.log(2 <= 1)//lessthen equal
+//console.log(2 >= 1)// greaterthen equal
+//console.log(2 == 1) //equal equal
+//console.log(2 != 1) // not equal
+
+console.log("2" > 1); //output is true js auto convert string to number but 
+console.log("02" > 1); 
+// true but whenever we use this type of camparision out not predictable so, prefer same datypes when use comparision
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+
+// strict check ===
+console.log("2"=== 2); //false  because datatypes is not same
+
+//note : always use clean code never use confusing code 
