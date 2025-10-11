a117f6153303b34a31dbf158f0be0527a957d7c8
Lubnacoder <khanlubna34069@gmail.com>
2025-10-11 02:23:34 +0000

    basics/05_datatypessummary.js

--- basics/05_datatypessummary.js
+++ basics/05_datatypessummary.js
@@ -13,11 +13,11 @@ let userEmail ; // undefined
 
 const id = Symbol('123')
 const anotherID = Symbol('123');
-console.log(anotherID);
+//console.log(anotherID);
 
 //const bigNumber = 56565656564896565n;
 const bigNumber = BigInt(56565656564896565);
-console.log(typeof(bigNumber));
+//console.log(typeof(bigNumber));
 
 
 // Reference type or Non premitive 
@@ -25,7 +25,7 @@ console.log(typeof(bigNumber));
 
 //1 array
 const cars = ["toyota fortuner", "maruti", "honda city", "bugati"];
-console.log(cars);
+//console.log(cars);
 
 // 2 obeject
 let myobj = {
@@ -34,20 +34,45 @@ let myobj = {
     phone : '564565663',
     email : "l@gmail.com"
 }
-console.log(myobj);
+//console.log(myobj);
 
 //3 function 
 
-function myfunc()
-{
- console.log("hello lubna");
-}
-myfunc();
+// function myfunc()
+// {
+//  console.log("hello lubna");
+// }
+// myfunc();
 
-const myfunc = function ()
-{
-    console.log("hello lubna");
+// const myfunc = function ()
+// {
+//     console.log("hello lubna");
+// }
+
+//================================  memory types   ===================================================\\
+
+// Stack(primitive) , Heap (non-primitive)
+let myYoutubename = "lubnacodes" // stack store this value first  
+
+let anothername = myYoutubename  
+console.log(myYoutubename);  //first stack value
+
+anothername = "newchannnel";
+console.log(anothername); // now new value is inserted in stact 
+
+
+// ===== Example of Heap =======
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ypi"
 }
+// this wiil copy and store in heap memory like a container and userOne is going in stack
+
+let usertwo = userOne
+
+usertwo.email="l@gmail.com"
+console.log(userOne.email);
+console.log(usertwo.email);
 
 
 
